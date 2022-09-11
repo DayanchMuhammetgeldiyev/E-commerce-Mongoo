f6f630576be30b1994f0da6aea9e7398bc8fdc63
DayanchM08 <muhammetgeldiyevdayanc@gmail.com>
2022-09-11 19:04:41 +0300

    Mongo

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJs = require("crypto-js");
+const jwt = require("jsonwebtoken"); 
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -24,27 +25,41 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login", async (req, res)=>{
-   try {
-        const user = await User.findOne({username: req.body.username}) ;
-        !user && res.status(401).json("Wrong Username");
-
-        const hashedPassword = CryptoJs.AES.decrypt(
-          user.password, 
-          process.env.PASS_SEC);
-
-        const password = hashedPassword.toString(CryptoJs.enc.Utf8);
-
-        password !== req.body.password && 
-        res.status(401).json("Wrong Password");
-          
-          const { password, ...others } = user;
-
-        res.status(200).json(others);
-   } catch (err) {
-          res.status(500).json(err);
-   }
-});
+router.post('/login', async (req, res) => {
+          try{
+              const user = await User.findOne(
+                  {
+                      userName: req.body.user_name
+                  }
+              );
+      
+              !user && res.status(401).json("Wrong User Name");
+      
+              const hashedPassword = CryptoJS.AES.decrypt(
+                  user.password,
+                  process.env.PASS_SEC
+              );
+              const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+              const inputPassword = req.body.password;
+              originalPassword != inputPassword && 
+                  res.status(401).json("Wrong Password");
+              const accessToken = jwt.sign(
+              {
+                  id: user._id,
+                  isAdmin: user.isAdmin,
+              },
+              process.env.JWT_SEC,
+                  {expiresIn:"3d"}
+              );
+        
+              const { password, ...others } = user._doc;  
+              res.status(200).json({...others, accessToken});
+      
+          }catch(err){
+              res.status(500).json(err);
+          }
+      
+      });
 
 
 
