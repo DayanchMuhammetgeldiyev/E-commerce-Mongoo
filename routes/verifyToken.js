5a04b8e16ac6a226ad79511f55b961388ef73147
DayanchM08 <muhammetgeldiyevdayanc@gmail.com>
2022-09-11 22:21:21 +0300

    Mongo

---
+++ routes/verifyToken.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (res, req, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) {
+        res.status(403).json("Token is not valid");
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("Not authenticated!");
+  }
+};
+
+const verifyTokenAndAuthorization = (res, req, next) => {
+  verifyToken(res, req, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
